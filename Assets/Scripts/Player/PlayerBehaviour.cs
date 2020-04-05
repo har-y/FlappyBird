4cb38cf5edcb38e63d711bfae6db960488bf63b7
har-y <jakub.harenda@gmail.com>
2020-04-05 18:27:00 +0200

    Add: Player (Faby) - Fly Up and Gravity Setup

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public float velocity = 1f;
+
+    private Rigidbody2D _rB;
+
+    private void Awake()
+    {
+        _rB = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,9 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rB.velocity = Vector2.up * velocity;
+        }
     }
 }
