9c9bb29f7aa111a78538c1555d715ce996dcaec5
har-y <jakub.harenda@gmail.com>
2020-04-05 19:18:46 +0200

    Add: Obstacle - C# Script Behaviour

--- Assets/Scripts/Pipe/ObstacleSpawnerBehaviour.cs
+++ Assets/Scripts/Pipe/ObstacleSpawnerBehaviour.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ObstacleSpawnerBehaviour : MonoBehaviour
 {
+    public GameObject pipePrefab;
+    public GameObject container;
+
+    public float nextSpawnInSeconds = 1f;
+    public float distance;
+
+    private float _timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,17 @@ public class ObstacleSpawnerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_timer > nextSpawnInSeconds)
+        {
+            GameObject tmp = Instantiate(pipePrefab);
+            tmp.transform.position = transform.position + new Vector3(0f, Random.Range(-distance, distance), 0f);
+            tmp.transform.parent = container.transform;
+
+            _timer = 0;
+
+            Destroy(tmp, 3f);
+        }
+
+        _timer += Time.deltaTime;
     }
 }
